e5b133af7301c286887f22981ddb766980e515be
Chikitori <tinh258456@gmail.com>
2025-08-16 13:21:52 +0700

    Fix build import assets

--- src/utils/audio.ts
+++ src/utils/audio.ts
@@ -1,7 +1,7 @@
 import { useCharacterStore } from "@/stores/characterStore";
 
 // const audioPath = `${audioAssetRoot}/${char.id}/${ANIMATION_TYPE_BASE_PATH[store.animationCategory]}`
-const assetRoot = '/assets/audios';
+const assetRoot = `${import.meta.env.BASE_URL}assets/audios`;
 
 let lastNumber: number | null = null;
 
